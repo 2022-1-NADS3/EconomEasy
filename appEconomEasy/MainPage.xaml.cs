d6e949bac136dd35fcbb771804c9ff26b1bb1eb7
21022697 <21022697@302-09>
2022-04-25 22:39:03 -0300

    Adicionar arquivos de projeto.

---
+++ appEconomEasy/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Teste_5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (txtLogin.Text == "admin" && txtSenha.Text == "admin")
+            {
+                DisplayAlert("Login efetuado", "Usuário identificado", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Ocorreu um erro!", "Usuário não identificado", "Ok");
+            }
+        }
+
+        private void Reconhecer_Tap(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
