9a84e35a4dd2707d5d88e88c617ed5e4a8c4e8ff
Erika Rocha de Barros <erika.barros@edu.fecap.br>
2022-05-29 23:47:11 -0300

    tentativaheader

---
+++ src/Frontend/appEconomEasy/Menu.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Reporting.Map.WebForms.BingMaps;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using appEconomEasy.ListaMenu;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace appEconomEasy
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : MasterDetailPage
+    {
+        private ObservableCollection<ListaMenu> listavMenu;
+
+        public Menu()
+        {
+            InitializeComponent();
+            listavMenu = ListaMenu.GetListaMenu();
+            lvItens.ItemSource = listavMenu;
+        }
+
+        private void selecaoItens (object sender, SelectedItemChangedEventArgs e)
+        {
+            var pagina = e.SelectedItem as ListaMenu;
+            Detail = new modeloPagina(pagina);
+            IsPresented = false;
+        }
+
+
+    }
+}
--- src/Frontend/appEconomEasy/Modelos/CadastroUsuario.xaml.cs
+++ src/Frontend/appEconomEasy/Modelos/CadastroUsuario.xaml.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace appEconomEasy
+namespace appEconomEasy.Modelos
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Menu : ContentPage
+    public partial class CadastroUsuario : ContentPage
     {
-        public Menu()
+        public CadastroUsuario()
         {
             InitializeComponent();
+            BindingContext = pagina;
         }
     }
 }
