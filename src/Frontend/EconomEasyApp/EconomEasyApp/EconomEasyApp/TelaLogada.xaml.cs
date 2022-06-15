017fc972e1a08465c86973dc85c22f52b7495009
Willians Souza <88861777+Souzawil@users.noreply.github.com>
2022-06-14 22:45:13 -0300

    Atualização graficos 2

--- src/Frontend/EconomEasyApp/EconomEasyApp/EconomEasyApp/TelaLogada.xaml.cs
+++ src/Frontend/EconomEasyApp/EconomEasyApp/EconomEasyApp/TelaLogada.xaml.cs
@@ -140,6 +140,76 @@ namespace EconomEasyApp
                 ValueLabelColor = SKColor.Parse("#808080"),
             }
         };
+        List<Entry> entries4 = new List<Entry>
+        {
+            new Entry(460)
+            {
+                Label = "Itaú",
+                ValueLabel = "1420",
+                Color = SKColor.Parse("#FFD700"),
+                TextColor = SKColor.Parse("#FFD700"),
+                ValueLabelColor = SKColor.Parse("#FFD700"),
+            },
+            new Entry(570)
+            {
+                Label = "Nu bank",
+                ValueLabel = "3570",
+                Color = SKColor.Parse("#0000FF"),
+                TextColor = SKColor.Parse("#0000FF"),
+                ValueLabelColor = SKColor.Parse("#0000FF"),
+            },
+            new Entry(4000)
+            {
+                Label = "Mar/22",
+                ValueLabel = "1257",
+                Color = SKColor.Parse("#FF0000"),
+                TextColor = SKColor.Parse("#FF0000"),
+                ValueLabelColor = SKColor.Parse("#FF0000"),
+            },
+        };
+        List<Entry> entries5 = new List<Entry>
+        {
+            new Entry(800)
+            {
+                Label = "Casa",
+                ValueLabel = "800",
+                Color = SKColor.Parse("#FFD700"),
+                TextColor = SKColor.Parse("#FFD700"),
+                ValueLabelColor = SKColor.Parse("#FFD700"),
+            },
+            new Entry(700)
+            {
+                Label = "Veículo",
+                ValueLabel = "700",
+                Color = SKColor.Parse("#0000FF"),
+                TextColor = SKColor.Parse("#0000FF"),
+                ValueLabelColor = SKColor.Parse("#0000FF"),
+            },
+            new Entry(200)
+            {
+                Label = "Entretenimento",
+                ValueLabel = "200",
+                Color = SKColor.Parse("#FF0000"),
+                TextColor = SKColor.Parse("#FF0000"),
+                ValueLabelColor = SKColor.Parse("#FF0000"),
+            },
+            new Entry(1200)
+            {
+                Label = "Educação",
+                ValueLabel = "1200",
+                Color = SKColor.Parse("#008000"),
+                TextColor = SKColor.Parse("#008000"),
+                ValueLabelColor = SKColor.Parse("#008000"),
+            },
+            new Entry(100)
+            {
+                Label = "Salário",
+                ValueLabel = "-7000",
+                Color = SKColor.Parse("#808080"),
+                TextColor = SKColor.Parse("#808080"),
+                ValueLabelColor = SKColor.Parse("#808080"),
+            }
+        };
         public TelaLogada()
         {
             InitializeComponent();
@@ -148,6 +218,8 @@ namespace EconomEasyApp
             Chart1.Chart = new RadialGaugeChart { Entries = entries};
             Chart2.Chart = new BarChart {Entries = entries2 };
             Chart3.Chart = new LineChart { Entries = entries3 };
+            Chart4.Chart = new PieChart { Entries = entries4 };
+            Chart4.Chart = new PointChart { Entries = entries5 };
         }
 
         private void AddAddInfFin(object sender, EventArgs e)
